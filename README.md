ba4386230628d1679904a48f73d931af161cbe7a
xiro-bit <xirone@ciencias.unam.mx>
2026-03-11 00:51:10 -0600

    se agrego completa la practica 2 aunque en cabal test aun me marca algunos errores

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Lógica Computacional 2026-2
 
-## Práctica 2
+## Práctica 1
 
 Para trabajar sobre esta base, tienen que hacer un fork de este repositorio y trabajar sobre él.
 
-Únicamente modifiquen el archivo Practica02.hs que se encuentra en el directorio src. Si quieren modificar las pruebas o agregar más, pueden preguntarme con confianza y les explico como modificarlas.
+Únicamente modifiquen el archivo Practica01.hs que se encuentra en el directorio src. Si quieren modificar las pruebas o agregar más, pueden preguntarme con confianza y les explico como modificarlas.
 
-Deben tener instalado el compilador de Haskell para poder probar su práctica. Para ello deben colocarse en el directorio src y ejecutar el comando `ghci Practica02.hs`.
+Deben tener instalado el compilador de Haskell para poder probar su práctica. Para ello deben colocarse en el directorio src y ejecutar el comando `ghci Practica01.hs`.
 
 Si quieren probar su práctica haciendo uso de las pruebas unitarias que les estoy pasando, tienen que ejecutar los siguientes comandos desde el directorio donde se encuentra este ReadMe:
 ```
@@ -16,19 +16,20 @@ cabal test
 
 El primero es para compilar y el segundo es para ejecutar las pruebas unitarias.
 
-Si no les llegan a funcionar, es posible que el problema es que tengan una versión diferente de cabal y de ghc. Si ese es el caso, pueden ejecutar el comando `ghc-pkg list base` para reemplazar la versión base que viene en el archivo .cabal en las líneas 70 y 102.
+Si no les llegan a funcionar, es posible que el problema es que tengan una versión diferente de cabal. Si es el caso, modifiquen el archivo `.cabal` con la versión que tienen o escríbanme para que les diga cuáles líneas modificar.
 
 ## Integrantes
 
 En esta sección deben eliminar esta línea de texto, borrar la leyenda "Integrante n" y escribir su nombre empezando por apellidos y su número de cuenta.
 
-+ Integrante 1
-    - No. de Cuenta: 
-+ Integrante 2
-    - No. de Cuenta: 
-+ Integrante 3
-    - No. de Cuenta: 
++ García Hernandez Greta Nicole
+     - No. de Cuenta: 322136313
++ Pérez García Blanca Xirone
+    - No. de Cuenta: 314213989
+
 
 ## Comentarios
 
-Recuerden que opcionalmente pueden agregar un comentario (ver los lineamientos de entrega). Si es el caso, borren esta línea y dejen su comentario.
+Tuvimos varios errores en las pruebas unitarias en las funciones de estadosPosibles y modelos porque en la formula Cg tenian los mismos resultados pero en distinto orden anexo imágenes en la carpeta RESULTADOS_TEST_UNITARIOS, además de la funcion sonEquivalentes y consecuenciaLogica que se definen a partir de las anteriores, estos son los 4 errores que se muestran en las pruebas unitarias cuando probamos cabal test. 
+
+Tambien usamos las funciones de filter, elem y all para ahorrar trabajo, pero supongo que era crearlas desde cero, una disculpa. 
--- src/Practica02.hs
+++ src/Practica02.hs
@@ -31,37 +31,70 @@ type Estado = [String]
 
 --Ejercicio 1
 variables :: Prop -> [String]
-variables = undefined
+variables p = auxrepetidos (auxVariables p)
+
+--Reune las variables
+auxVariables :: Prop -> [String]
+auxVariables (Cons True) = []
+auxVariables (Cons False) = []
+auxVariables (Var p) = [p]
+auxVariables (Not p) = auxVariables p
+auxVariables (Or p q) = auxVariables p ++ auxVariables q
+auxVariables (And p q) = auxVariables p ++ auxVariables q
+auxVariables (Impl p q) = auxVariables p ++ auxVariables q
+auxVariables (Syss p q) = auxVariables p ++ auxVariables q
+
+--Filtra las variables repetidas
+auxrepetidos :: [String] -> [String]
+auxrepetidos [] = []
+auxrepetidos (x:xs) = x : auxrepetidos (filter (/= x) xs)
+
 
 --Ejercicio 2
 interpretacion :: Prop -> Estado -> Bool
-interpretacion = undefined
+interpretacion (Cons True) estado = True
+interpretacion (Cons False) estado = False
+interpretacion (Var p) estado = elem p estado
+interpretacion (Not fp) estado = not(interpretacion fp estado)
+interpretacion (Or fp fq) estado = interpretacion fp estado || interpretacion fq estado
+interpretacion(And fp fq) estado = interpretacion fp estado && interpretacion fq estado
+interpretacion (Impl fp fq) estado = not(interpretacion fp estado) || interpretacion fq estado 
+interpretacion (Syss fp fq) estado= interpretacion fp estado == interpretacion fq estado  
+
+
+
 
 --Ejercicio 3
 estadosPosibles :: Prop -> [Estado]
-estadosPosibles = undefined
+estadosPosibles fp = conjPotencia(variables (fp)) 
+-- IMPORTANTE!!! en el caso la formula Cg maneja un distinto orden pero son el mismo numeros de conjuntos solo
+--- en distinto orden, por lo que al hacer los test unitarios marcan error anexo captura--
+
 
 --Ejercicio 4
 modelos :: Prop -> [Estado]
-modelos = undefined
+modelos fp = [estado | estado <- estadosPosibles fp, interpretacion fp estado]
+-- IMPORTANTE!!!mismo caso del ejercicio anterior, la formula Cg maneja un distinto orden pero son el mismo numeros de conjuntos solo
+--- en distinto orden, por lo que al hacer los test unitarios marcan error anexo captura--
+
 
 --Ejercicio 5
 sonEquivalentes :: Prop -> Prop -> Bool
-sonEquivalentes = undefined
+sonEquivalentes fp fq = modelos fp == modelos fq
+-- se acarrea el mismo problema de las funciones anteriores como tienen distinto orden marca como falso algo que si es equivalente
+
 
 --Ejercicio 6 
 tautologia :: Prop -> Bool
-tautologia = undefined
+tautologia fp = estadosPosibles fp == modelos fp
 
 --Ejercicio 7
 contradiccion :: Prop -> Bool
-contradiccion = undefined
+contradiccion fp = modelos fp == []
 
 --Ejercicio 8
 consecuenciaLogica :: [Prop] -> Prop -> Bool
-consecuenciaLogica = undefined
-
-
+consecuenciaLogica premisas conclusion = all (`elem` modelos conclusion) (filter (\estado -> all (\p -> interpretacion p estado) premisas) (estadosPosibles conclusion))
 --Funcion auxiliar
 conjPotencia :: [a] -> [[a]]
 conjPotencia [] = [[]]
